c655590c49cf09fa4355dd79b24c0f5402b0303d
Bernardo Covas <bernardo.covas@outlook.com>
2019-03-18 05:33:38 +0000

    updated mac addresses

--- src/mac_lstnr_config.h
+++ src/mac_lstnr_config.h
@@ -1,7 +1,7 @@
 #define MAC_LISTNR_KNOWN_CONFIG {                     \
-    { .name="jp.covas" , .mac="94:65:2d:a7:fd:6d" },  \
+    { .name="jp.covas" , .mac="d4:dc:cd:9a:7a:2f" },  \
     { .name="m.santos" , .mac="94:65:2d:a7:fd:6d" },  \
-    { .name="p.lima"   , .mac="94:65:2d:a7:fd:6d" },  \
-    { .name="m.covas"  , .mac="94:65:2d:a7:fd:6c" },  \
+    { .name="p.lima"   , .mac="88:83:22:77:4d:ae" },  \
+    { .name="m.covas"  , .mac="c0:ee:fb:d1:4b:43" },  \
     { .name="b.covas"  , .mac="94:65:2d:a7:fd:6b" },  \
 }
