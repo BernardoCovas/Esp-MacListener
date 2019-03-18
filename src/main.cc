9d000a178ca6cc750dd17ebf7080ce16735e74de
Bernardo Covas <bernardo.covas@outlook.com>
2019-03-18 05:13:27 +0000

    added blinking when known mac is seen

--- src/main.cc
+++ src/main.cc
@@ -7,12 +7,26 @@
 #include "mac_lstnr_wifi.h"
 #include "mac_lstnr_display.h"
 
+#ifdef CONFIG_BLINK_GPIO
+#define	LED_GPIO_PIN CONFIG_BLINK_GPIO
+#else
+#define	LED_GPIO_PIN GPIO_NUM_2 // Default for esp32
+#endif
+
+#define BLINK_PATTERN_NEW_PACKET_BADADDR 3, 33
+#define BLINK_PATTERN_NEW_PACKET 1, 100
+#define BLINK_PATTERN_NEW_ADDR 2, 50
+
 void rx_callback(void *buff, wifi_promiscuous_pkt_type_t type);
+void blinkPattern(uint16_t num_blinks, uint32_t delay_ms);
+
 mac_listnr_t *listener;
 int curr_channel = 0;
 
 
 void setup() {
+
+	gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
 
 	listener = mac_listnr_init();
 	mac_listnr_wifi_init(&rx_callback);
@@ -45,5 +59,17 @@ void rx_callback(void *buff, wifi_promiscuous_pkt_type_t type)
 	if (index >= 0) {
 		listener->known[index]._hits  ++;
 		listener->known[index]._secs = 0;
+		blinkPattern(BLINK_PATTERN_NEW_PACKET_BADADDR);
 	}
+}
+
+void blinkPattern(uint16_t num_blinks, uint32_t delay_ms) {
+	
+	for (int i=0; i<num_blinks; i++) {
+		gpio_set_level(LED_GPIO_PIN, 1);
+		vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+		gpio_set_level(LED_GPIO_PIN, 0);
+		vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+	}
+
 }
